af7d8b37fffc6deaa351b28bc927efada2b36b02
GDJEvans <gdjevans@gmail.com>
2022-05-23 19:02:42 +0100

    Day 30 Begin Javascript Learn.

---
+++ 100DaysOfCode/Project_8_Javascript/app.js
@@ -0,0 +1,13 @@
+    /*let age = 32;
+    let greetingText = 'Hi, my name is Max!';
+    alert(greetingText);
+    alert(greetingText);
+    greetingText = 'Hi, I\'m really Max';
+    alert(greetingText);
+    alert(age);*/
+
+    let age = 32;
+    let userName = 'Max';
+    let hobbies = ['Sports', 'Cooking', 'Reading'];
+    let job = { title: 'Developer', place: 'New York', salary: 50000};
+    alert(hobbies[1]);
