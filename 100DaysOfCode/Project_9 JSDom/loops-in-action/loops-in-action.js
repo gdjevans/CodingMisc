b8180bfad8bd6540a6cd4ce50bd9979de3cf0570
GDJEvans <gdjevans@gmail.com>
2022-06-01 22:47:46 +0100

    Day 39 - Finishing Loops

---
+++ 100DaysOfCode/Project_9 JSDom/loops-in-action/loops-in-action.js
@@ -0,0 +1,101 @@
+//First Example: Sum Numbers
+
+const CalculateSumButtonElement = document.querySelector('#calculator button');
+
+function calculateSum() {
+    const userNumberInputElement = document.getElementById('user-number');
+    const enteredNumber = userNumberInputElement.value;
+
+    let sumUpToNumber = 0;
+
+    for (let i =0; i <= enteredNumber; i++) {
+        sumUpToNumber = sumUpToNumber + i;
+    }
+
+    const outputResultElement = document.getElementById('calculated-sum');
+
+    outputResultElement.textContent = sumUpToNumber;
+    outputResultElement.style.display = 'block';
+}
+
+CalculateSumButtonElement.addEventListener('click', calculateSum);
+
+//Highlight Links 
+
+const highlightLinksButtonElement = document.querySelector('#highlight-links button');
+
+function highlightLinks() {
+    const anchorElements = document.querySelectorAll('#highlight-links a');
+
+    for (const anchorElement of anchorElements) {
+        anchorElement.classList.add('highlight');
+    }
+}
+
+highlightLinksButtonElement.addEventListener('click', highlightLinks);
+
+//DisplayUserData
+
+const dummyUserData = {
+    firstName: 'Oswald',
+    lastName: 'Kerplunk',
+    age: '64'
+}
+
+const displayUserDataButtonElement = document.querySelector('#user-data button');
+
+function displayUserData() {
+    const outputDataElement = document.getElementById('output-user-data');
+
+    outputDataElement.innerHTML = '';
+
+    for (const key in dummyUserData) {
+        const newUserDataListItemElement = document.createElement('li');
+        const outputText = key.toUpperCase() + ': ' + dummyUserData[key];
+        newUserDataListItemElement.textContent = outputText;
+        outputDataElement.append(newUserDataListItemElement);
+    }
+}
+
+displayUserDataButtonElement.addEventListener('click', displayUserData);
+
+//Statistics - Roll The Dice
+
+const rollDiceButtonElement = document.querySelector('#statistics button');
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function deriveNumberOfDiceRolls () {
+    const targetNumberInputElement = document.getElementById('user-target-number');
+    const diceRollsListElement = document.getElementById('dice-rolls');
+
+    const enteredNumber = targetNumberInputElement.value; 
+    diceRollsListElement.innerHTML = '';
+
+    let hasRolledTargetNumber = false;
+    let numberOfRolls = 0;
+
+    while (!hasRolledTargetNumber) {
+        const rolledNumber = rollDice();
+      /*   if (rolledNumber == enteredNumber){
+            hasRolledTargetNumber = true;
+        } */
+
+        numberOfRolls++;
+        const newRollListElement = document.createElement('li');
+        const outputText = 'Roll ' + numberOfRolls + ': ' + rolledNumber;
+        newRollListElement.textContent = outputText;
+        diceRollsListElement.append(newRollListElement);
+        hasRolledTargetNumber = rolledNumber == enteredNumber;
+    }
+
+    const outputTotalRollsElement = document.getElementById('ouput-total-rolls');
+    const outputTargetNumberElement = document.getElementById('output-target-number');
+
+    outputTargetNumberElement.textContent = enteredNumber;
+    outputTotalRollsElement.textContent = numberOfRolls;
+}
+
+rollDiceButtonElement.addEventListener('click', deriveNumberOfDiceRolls);
