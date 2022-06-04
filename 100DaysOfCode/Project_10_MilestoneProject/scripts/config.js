5da088af0736750eb207ca94a4fd178433668693
GDJEvans <gdjevans@gmail.com>
2022-06-04 23:27:48 +0100

    Partial day 42 midway through start of Javascript

--- 100DaysOfCode/Project_10_MilestoneProject/scripts/config.js
+++ 100DaysOfCode/Project_10_MilestoneProject/scripts/config.js
@@ -6,6 +6,8 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
@@ -14,6 +16,7 @@ function savePlayerConfig(event) {
     const enteredPlayername = formdata.get('playername').trim();
 
     if (!enteredPlayername ){
+        event.target.firstElementChild.classList.add('error');
         errorsOutputElement.textContent = 'Please enter a valid name!';
         return;
     }
