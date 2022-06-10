8df69c5e4d5e64359d3f45d8f7cf2b1a782a35e6
GDJEvans <gdjevans@gmail.com>
2022-06-10 17:26:27 +0100

    Day 48 - Express.JS

--- 100DaysOfCode/Project_12_NodeJS/app.js
+++ 100DaysOfCode/Project_12_NodeJS/app.js
@@ -1,20 +1,34 @@
-const http = require ('http');
+const fs = require('fs');
+const path = require('path');
 
-function handleRequest(request, response) {
+const express = require ('express');
 
-    if (request.url === '/currenttime') {
-        response.statusCode = 200;
-        response.end('<h1>'+ new Date().toISOString() +'</h1>');
-    }else if (request.url === '/') {
-        response.statusCode = 200;
-        response.end('<h1>Hello World!</h1>');
-    }
-    // locaLHOST:3000/CURRENTTIME
-    //localhost:3000
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.get('/currenttime', function(request, response) {
+    response.send('<h1>'+ new Date().toISOString() +'</h1>');
+});
+
+app.get('/', function(request, response) {
+    response.send('<form action ="/store-user" method="POST"><label>Your Name:</label><input type="text" name="username"><button>Submit</button></form>');
+});
+
+app.post('/store-user', function(request, response) {
+    const userName = request.body.username;
+    const filePath = path.join(__dirname, 'data', 'users.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    existingUsers.push(userName);
     
-}
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers))
+    response.send('<h1>Username stored!</h1>');
+});
 
-const server = http.createServer(handleRequest);
 
-server.listen(3000);
+
+app.listen(3000)
 
