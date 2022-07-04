c2ccc281fb6acb05570f6fdce2570caa41cd4a4e
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-07-04 15:57:13 +0100

    Day 72 - Authentication with Sessions

--- 100DaysOfCode/Project_20_Auth/routes/demo.js
+++ 100DaysOfCode/Project_20_Auth/routes/demo.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const db = require('../data/database');
 const { getDb } = require('../data/database');
+const session = require('express-session');
 
 const router = express.Router();
 
@@ -11,7 +12,22 @@ router.get('/', function (req, res) {
 });
 
 router.get('/signup', function (req, res) {
-  res.render('signup');
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      confirmEmail: '',
+      password: ''
+    };
+
+    
+  }
+
+  req.session.inputData = null;
+
+  res.render('signup', { inputDaa: sessionInputData });
 });
 
 router.get('/login', function (req, res) {
@@ -31,8 +47,20 @@ router.post('/signup', async function (req, res) {
     enteredPassword.trim() < 6 || 
     enteredEmail !== enteredConfirmEmail || 
     !enteredEmail.includes('@')) {
-      console.log('Incorrect sign up data!')
-      return res.redirect('/signup');
+      
+      req.session.inputData = {
+        hasError: true,
+        message: 'Invalid input - please check your data!',
+        email: enteredEmail,
+        confirmEmail: enteredConfirmEmail,
+        password: enteredPassword
+      };
+
+      req.session.save(function () {
+        return res.redirect('/signup');
+      });
+      
+      return;
   }
 
   const existingUser = await db.getDb().collection('users').findOne({email: enteredEmail});
@@ -73,15 +101,24 @@ router.post('/login', async function (req, res) {
     return res.redirect('/login');
   }
 
-  console.log('User is authenticated!');
-  res.redirect('/admin');
+  req.session.user = { id: existingUser._id, email: existingUser.email };
+  req.session.isAuthenticated = true;
+  req.session.save(function () {
+    res.redirect('/admin');
+  });
 });
 
 router.get('/admin', function (req, res) {
-  //Check the user "ticket"
+  if (!req.session.isAuthenticated) { //if (!req.session.user)
+    return res.status(401).render('401');
+  }
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) {});
+router.post('/logout', function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  res.redirect('/');
+});
 
 module.exports = router;
