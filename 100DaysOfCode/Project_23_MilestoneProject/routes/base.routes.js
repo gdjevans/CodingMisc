c68ee4273ae6f7a62d1ddd85934d83c9d2257acc
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-07-20 08:05:22 +0100

    Day 88 - Product Details for Customers

--- 100DaysOfCode/Project_23_MilestoneProject/routes/base.routes.js
+++ 100DaysOfCode/Project_23_MilestoneProject/routes/base.routes.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res) {
     res.redirect('/products');
 });
 
+router.get('/401', function(req, res) {
+   res.status(401).redirect('shared/401'); 
+})
+
+router.get('/403', function(req, res) {
+    res.status(403).redirect('shared/403'); 
+ })
+
 module.exports = router;
