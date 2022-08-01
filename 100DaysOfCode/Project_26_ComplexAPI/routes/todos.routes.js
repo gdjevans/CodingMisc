5f82c74ede0abbbcec5ffca10d6857362791dbe8
DESKTOP-6FBGBC6\GDJEva <gdjevans@hotmail.com>
2022-08-01 19:22:38 +0100

    Day 97 - API's and Web Services

---
+++ 100DaysOfCode/Project_26_ComplexAPI/routes/todos.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { TopologyDescription } = require('mongodb');
+
+const todosController = require('../controllers/todos.controller');
+
+const router = express.Router();
+
+router.get('/', todosController.getAllTodos);
+
+router.post('/', todosController.addTodo);
+
+router.patch('/:id', todosController.updateTodo);
+
+router.delete('/:id', todosController.deleteTodo);
+
+module.exports = router;
